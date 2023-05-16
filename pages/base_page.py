f6b6a254427519fb928651d2b6cdef060433ea29
Eugene Golnev <eugenegolnev1993@gmail.com>
2023-05-16 23:50:20 +0200

    Add a method in the BasePage to check the element on the page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,21 @@
+from typing import Union
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser: Union[webdriver.Chrome, webdriver.Firefox], url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        """how to search (css, id, xpath и тд) and what (selector string). """
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
